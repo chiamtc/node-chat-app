0ea560db97b876debe7cc50b662473d4c6176d12
DESKTOP-D6SFLPS\chiamtc <chiamcheng@gmail.com>
2018-09-15 14:07:05 +1000

    added connect and disconnect envet handler on both ends using socketio

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+//register an event listener
+//in this example, we're listneing to connection event - when a new user conencts
+io.on('connection', (socket) => {
+    console.log('new user connected');
 
+    socket.on('disconnect', ()=>{
+        console.log('user was disconnected')
+    });
+});
 app.use(express.static(publicPath));
-app.listen(port, ()=>{
-    console.log('server is up on ' + port);
-})
+server.listen(port, () => {
+    console.log(`server is up on ${port}`);
+});
